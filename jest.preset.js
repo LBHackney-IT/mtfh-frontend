f25ef2139c8e60c2f9101e0d87ceb42e7db2f05d
George Wilde <george.wilde@amido.com>
2021-04-15 20:40:19 +0100

    chore(repo): add global level config to Nx

--- jest.preset.js
+++ jest.preset.js
@@ -1,3 +1,17 @@
 const nxPreset = require('@nrwl/jest/preset');
 
-module.exports = { ...nxPreset };
+module.exports = {
+    ...nxPreset,
+    coverageReporters: ['text', 'html', 'cobertura'],
+    reporters: [
+        'default',
+        [
+            'jest-junit',
+            {
+                outputDirectory: '<rootDir>',
+                outputName: 'test-report.xml',
+            },
+        ],
+    ],
+    verbose: true,
+};
