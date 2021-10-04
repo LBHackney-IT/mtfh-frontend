90b1b5fce5940b0114ef1c7f3bbc6c75ff89cfee
srcrustaceo <ureciomunicio@deloitte.co.uk>
2021-10-04 11:03:53 +0200

    fix(naming): adds v1

--- packages/test-utils/src/mocks/asset/v1/handlers.ts
+++ packages/test-utils/src/mocks/asset/v1/handlers.ts
@@ -3,22 +3,22 @@ import { rest } from "msw";
 import { Asset } from "@mtfh/common/lib/api/asset/v1";
 import { generateMockAssetTenureV1, generateMockAssetV1 } from "./data";
 
-export const mockAsset = generateMockAssetV1();
+export const mockAssetV1 = generateMockAssetV1();
 const mockInactiveTenureV1 = generateMockAssetTenureV1({ isActive: false });
-export const mockAssetWithInactiveTenure: Asset = {
-  ...mockAsset,
+export const mockAssetWithInactiveTenureV1: Asset = {
+  ...mockAssetV1,
   tenure: mockInactiveTenureV1,
 };
-export const mockAssetLettableNonDwelling: Asset = {
-  ...mockAsset,
+export const mockAssetLettableNonDwellingV1: Asset = {
+  ...mockAssetV1,
   assetType: "LettableNonDwelling",
 };
-export const mockAssetInvalidAssetType: Asset = {
-  ...mockAsset,
+export const mockAssetInvalidAssetTypeV1: Asset = {
+  ...mockAssetV1,
   assetType: "invalidAssetType",
 };
 
-export const getAssetV1 = (data: any = mockAsset, code = 200) => {
+export const getAssetV1 = (data: any = mockAssetV1, code = 200) => {
   return rest.get("/api/v1/assets/:id", (req, res, ctx) =>
     res(ctx.status(code), ctx.json(data)),
   );
