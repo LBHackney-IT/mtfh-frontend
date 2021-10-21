26877ea48e2ffd26b02320f4e6af3c5e6cd76393
srcrustaceo <ureciomunicio@deloitte.co.uk>
2021-10-21 14:35:46 +0200

    fix(address api): data on handler

--- packages/test-utils/src/mocks/address/v1/handlers.ts
+++ packages/test-utils/src/mocks/address/v1/handlers.ts
@@ -8,8 +8,6 @@ export const getAddressV1 = (data: any = mockAddresses, code = 200) =>
   rest.get(`${config.addressApiUrlV1}/addresses`, (req, res, ctx) => {
     return res(
       ctx.status(code),
-      ctx.json(
-        typeof data === "function" ? data(req) : { data: { address: mockAddresses } },
-      ),
+      ctx.json(typeof data === "function" ? data(req) : { data: { address: data } }),
     );
   });
