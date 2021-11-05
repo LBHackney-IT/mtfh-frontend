b6ab855d9e5111a34aa5daf2f3bc6d1b4531b118
Unai Recio <crustaceo1@gmail.com>
2021-11-05 17:13:14 +0100

    Update/equality information mocks (#71)
    * feat(mocks): adds more reference data and improves equality information
    
    * feat(mocks): adds equality info reference data
    
    * removes yalc
    
    * chore(deps): removes yalc
    
    * chore(deps): update common
    
    * fix(equality details): replaces corrupted file
    
    Co-authored-by: srcrustaceo <ureciomunicio@deloitte.co.uk>

--- packages/test-utils/src/mocks/equality-information/v1/index.ts
+++ packages/test-utils/src/mocks/equality-information/v1/index.ts
@@ -1,2 +1,2 @@
-export * from "./handlers";
 export * from "./data";
+export * from "./handlers";
--- packages/test-utils/src/mocks/reference-data/v1/handlers.ts
+++ packages/test-utils/src/mocks/reference-data/v1/handlers.ts
@@ -1,9 +1,14 @@
 import { rest } from "msw";
 
-import { generateMockReferenceDataV1 } from "./data";
+import {
+  generateMockReferenceDataV1,
+  mockEqualityInformationReferenceData,
+} from "./data";
 
 import { ReferenceData } from "@mtfh/common/lib/api/reference-data/v1";
 import { config } from "@mtfh/common/lib/config";
+
+export const EQUALITY_INFORMATION_CATEGORY = "equality-information";
 
 export const mockReferenceDataV1 = generateMockReferenceDataV1();
 
@@ -24,3 +29,13 @@ export const getReferenceDataV1 = (data: any, code = 200) => {
     return res(ctx.status(code), ctx.json(response));
   });
 };
+
+export const getEqualityInformationReferenceData = (
+  data: any = mockEqualityInformationReferenceData,
+  code = 200,
+) =>
+  rest.get(`${config.referenceDataApiUrlV1}/reference-data`, (req, res, ctx) => {
+    if (req.url.search.indexOf(EQUALITY_INFORMATION_CATEGORY) !== -1)
+      return res(ctx.status(code), ctx.json(data));
+    return res(ctx.status(code), ctx.json({}));
+  });
