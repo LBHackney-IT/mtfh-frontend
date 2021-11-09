3fe72b5e57dc5927e8f343f40163996e64e629c4
Gareth Cozens <gareth.cozens@amido.com>
2021-11-09 13:51:05 +0000

    feat(test-utils): use generators and apply filtering to handler

--- packages/test-utils/src/mocks/reference-data/v1/handlers.ts
+++ packages/test-utils/src/mocks/reference-data/v1/handlers.ts
@@ -1,27 +1,10 @@
 import { rest } from "msw";
 
-import { TENURE_TYPES } from "../../../constants";
-import {
-  generateMockReferenceDataV1,
-  mockEqualityInformationReferenceData,
-} from "./data";
-
 import { ReferenceData } from "@mtfh/common/lib/api/reference-data/v1";
 import { config } from "@mtfh/common/lib/config";
 
-export const EQUALITY_INFORMATION_CATEGORY = "equality-information";
-
-export const mockReferenceDataV1 = generateMockReferenceDataV1();
-
-export const mockTenureTypeReferences: ReferenceData[] = TENURE_TYPES.map(
-  (referenceType) => ({
-    ...mockReferenceDataV1,
-    ...referenceType,
-  }),
-);
-
 export const getReferenceDataV1 = (
-  data?: ReferenceData[] | Record<string, ReferenceData[]>,
+  data: ReferenceData[] | Record<string, ReferenceData[]> = {},
   code = 200,
 ) => {
   return rest.get(`${config.referenceDataApiUrlV1}/reference-data`, (req, res, ctx) => {
@@ -29,9 +12,11 @@ export const getReferenceDataV1 = (
     const category = req.url.searchParams.get("category");
     const subCategory = req.url.searchParams.get("subCategory");
 
+    // If the data is an array, it could contain multiple categories and subCategories
     if (Array.isArray(data)) {
       response = data.reduce((accum, ref) => {
         if (ref.category !== category) return accum;
+        if (subCategory && subCategory !== ref.subCategory) return accum;
         if (!accum[ref.subCategory]) {
           accum[ref.subCategory] = [];
         }
@@ -39,34 +24,15 @@ export const getReferenceDataV1 = (
 
         return accum;
       }, {} as Record<string, ReferenceData[]>);
-    }
-
-    if (category === "tenure" && subCategory === "type") {
-      return res(
-        ctx.status(code),
-        ctx.json(response || { type: mockTenureTypeReferences }),
-      );
-    }
-    if (category === "equality-information") {
-      return res(
-        ctx.status(code),
-        ctx.json(response || mockEqualityInformationReferenceData),
-      );
-    }
-    if (category === "error-code") {
-      return res(ctx.status(code), ctx.json({}));
+      // If the data is a single object, it should only have a single category
+    } else if (subCategory) {
+      response = Object.entries(data).reduce((accum, [subKey, refs]) => {
+        if (subKey !== subCategory) return accum;
+        accum[subCategory] = refs;
+        return accum;
+      }, {} as Record<string, ReferenceData[]>);
     }
 
     return res(ctx.status(code), ctx.json(response));
   });
 };
-
-export const getEqualityInformationReferenceData = (
-  data: any = mockEqualityInformationReferenceData,
-  code = 200,
-) =>
-  rest.get(`${config.referenceDataApiUrlV1}/reference-data`, (req, res, ctx) => {
-    if (req.url.search.indexOf(EQUALITY_INFORMATION_CATEGORY) !== -1)
-      return res(ctx.status(code), ctx.json(data));
-    return res(ctx.status(code), ctx.json({}));
-  });
