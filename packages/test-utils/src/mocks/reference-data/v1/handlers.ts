d2ded4e50037cff5fe4ab974bf8df5d9209f9cb3
srcrustaceo <unai.recio@amido.com>
2021-11-09 13:19:22 +0100

    feat(reference data): returns based on categories

--- packages/test-utils/src/mocks/reference-data/v1/handlers.ts
+++ packages/test-utils/src/mocks/reference-data/v1/handlers.ts
@@ -1,5 +1,6 @@
 import { rest } from "msw";
 
+import { TENURE_TYPES } from "../../../constants";
 import {
   generateMockReferenceDataV1,
   mockEqualityInformationReferenceData,
@@ -12,20 +13,50 @@ export const EQUALITY_INFORMATION_CATEGORY = "equality-information";
 
 export const mockReferenceDataV1 = generateMockReferenceDataV1();
 
-export const getReferenceDataV1 = (data: any, code = 200) => {
-  let response = data;
+export const mockTenureTypeReferences: ReferenceData[] = TENURE_TYPES.map(
+  (referenceType) => ({
+    ...mockReferenceDataV1,
+    ...referenceType,
+  }),
+);
 
-  if (Array.isArray(data)) {
-    response = data.reduce((accum, ref) => {
-      if (!accum[ref.subCategory]) {
-        accum[ref.subCategory] = [];
-      }
-      accum[ref.subCategory].push(ref);
-
-      return accum;
-    }, {} as Record<string, ReferenceData[]>);
-  }
+export const getReferenceDataV1 = (
+  data?: ReferenceData[] | Record<string, ReferenceData[]>,
+  code = 200,
+) => {
   return rest.get(`${config.referenceDataApiUrlV1}/reference-data`, (req, res, ctx) => {
+    let response = data;
+    const category = req.url.searchParams.get("category");
+    const subCategory = req.url.searchParams.get("subCategory");
+
+    if (Array.isArray(data)) {
+      response = data.reduce((accum, ref) => {
+        if (ref.category !== category) return accum;
+        if (!accum[ref.subCategory]) {
+          accum[ref.subCategory] = [];
+        }
+        accum[ref.subCategory].push(ref);
+
+        return accum;
+      }, {} as Record<string, ReferenceData[]>);
+    }
+
+    if (category === "tenure" && subCategory === "type") {
+      return res(
+        ctx.status(code),
+        ctx.json(response || { type: mockTenureTypeReferences }),
+      );
+    }
+    if (category === "equality-information") {
+      return res(
+        ctx.status(code),
+        ctx.json(response || mockEqualityInformationReferenceData),
+      );
+    }
+    if (category === "error-code") {
+      return res(ctx.status(code), ctx.json({}));
+    }
+
     return res(ctx.status(code), ctx.json(response));
   });
 };
