51ffa11c6d909165246bfcfbd7bec570dacaa409
kymmeh <kymmehayley@gmail.com>
2021-11-10 14:41:24 +0000

    feat: fixing linting

--- packages/test-utils/src/mocks/repair/v1/handlers.ts
+++ packages/test-utils/src/mocks/repair/v1/handlers.ts
@@ -1,5 +1,7 @@
 import { rest } from "msw";
+
 import { mockRepairWorkOrders } from "./data";
+
 import { config } from "@mtfh/common/lib/config";
 
 export const getRepairWorkOrdersV1 = (data: any = mockRepairWorkOrders, code = 200) =>
