632f742c564c2c0d363384a59b9a5f8f204c0047
kymmeh <kymmehayley@gmail.com>
2021-12-02 15:51:34 +0000

    feat(test-utils): updating common

--- packages/test-utils/src/mocks/process/v1/data.ts
+++ packages/test-utils/src/mocks/process/v1/data.ts
@@ -2,28 +2,7 @@ import faker from "faker/locale/en";
 
 faker.seed(1);
 
-export interface Process {
-  id: string;
-  targetID: string;
-  relatedEntities: string[];
-  formData: object;
-  documents: string[];
-  processName: string;
-  currentState: ProcessState;
-  previousStates: ProcessState[];
-}
-
-export interface ProcessState {
-  stateName: string;
-  permittedTriggers: string[];
-  assignment: string;
-  processData: {
-    formData: object;
-    documents: string[];
-  };
-  createdAt: string;
-  updatedAt: string;
-}
+import { Process, ProcessState } from "@mtfh/common/lib/api/process/v1";
 
 export const generateMockProcessV1 = (data: Partial<Process> = {}): Process => {
   return {
@@ -38,9 +17,9 @@ export const generateMockProcessV1 = (data: Partial<Process> = {}): Process => {
     ),
     processName: faker.lorem.word(),
     currentState: generateMockProcessStateV1(),
-    previousStates: Array.from({ length: faker.datatype.number({ min: 1, max: 4 }) }).map(
-      () => generateMockProcessStateV1(),
-    ),
+    previousStates: Array.from({
+      length: faker.datatype.number({ min: 1, max: 4 }),
+    }).map(() => generateMockProcessStateV1()),
     ...data,
   };
 };
@@ -56,9 +35,9 @@ export const generateMockProcessStateV1 = (
     assignment: faker.lorem.word(),
     processData: {
       formData: {},
-      documents: Array.from({ length: faker.datatype.number({ min: 1, max: 4 }) }).map(
-        () => faker.datatype.uuid(),
-      ),
+      documents: Array.from({
+        length: faker.datatype.number({ min: 1, max: 4 }),
+      }).map(() => faker.datatype.uuid()),
     },
     createdAt: faker.date.past().toISOString(),
     updatedAt: faker.date.past().toISOString(),
