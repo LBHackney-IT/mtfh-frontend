07d744004a558f716783ba2f6d07aea2bdaa889c
kymmeh <kymmehayley@gmail.com>
2021-12-02 17:21:28 +0000

    feat(test-utils): updating method / adding get

--- packages/test-utils/src/mocks/process/v1/handlers.ts
+++ packages/test-utils/src/mocks/process/v1/handlers.ts
@@ -6,8 +6,17 @@ import { generateMockProcessV1 } from "./data";
 
 export const mockProcessV1 = generateMockProcessV1();
 
+export const getProcessV1 = (data: any = mockProcessV1, code = 200) => {
+  return rest.get(
+    `${config.processApiUrlV1}/process/:processName/:id`,
+    (req, res, ctx) => {
+      return res(ctx.status(code), ctx.json(data));
+    },
+  );
+};
+
 export const postProcessV1 = (data: any = mockProcessV1, code = 200) => {
-  return rest.get(`${config.processApiUrlV1}/process/:processName`, (req, res, ctx) => {
+  return rest.post(`${config.processApiUrlV1}/process/:processName`, (req, res, ctx) => {
     return res(ctx.status(code), ctx.json(data));
   });
 };
