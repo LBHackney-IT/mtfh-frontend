86fb312e24e422ae65a40b8861607f1a2d6481bd
humulla <43747248+humulla@users.noreply.github.com>
2023-04-28 12:20:52 +0100

    mock alerts

---
+++ packages/test-utils/src/mocks/cautionary-alerts/v1/handler.ts
@@ -0,0 +1,31 @@
+import { rest } from "msw";
+
+import { config } from "@mtfh/common/lib/config";
+
+
+export const mockAlertV1 = {
+    propertyReference: "12345",
+    uprn: null,
+    addressNumber: null,
+    alerts: [
+      {
+        dateModified: "12/02/2022",
+        modifiedBy: "GoogleSheet",
+        startDate: "12/02/2022",
+        endDate: null,
+        alertCode: null,
+        description: "Domestic Abuse Case - please seek advice",
+        reason: null,
+        assureReference: "123456",
+        personName: "FAKE_Alice FAKE_Rowe",
+        personId: null,
+      },
+    ],
+  };
+
+
+export const getAlertsV1 = (data: any = mockAlertV1, code = 200) => {
+  return rest.get(`${config.cautionaryApiUrlV1}/cautionary-alerts/properties-new/:id`, (req, res, ctx) =>
+    res(ctx.status(code), ctx.json(data)),
+  );
+};
