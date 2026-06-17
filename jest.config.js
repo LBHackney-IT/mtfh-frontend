4cef3cd842e6bbe5e24eccd12101ba758d83240f
Sam Drumm <sam.drumm@hackney.gov.uk>
2026-06-17 16:10:09 +0100

    test: set wide react support range for cypress

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,8 @@ const interceptorsRoot = path.join(
   "node_modules/@mswjs/interceptors/lib/node/interceptors",
 );
 
+const rootNodeModules = path.join(__dirname, "node_modules");
+
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
@@ -15,10 +17,14 @@ module.exports = {
   },
   moduleNameMapper: {
     "\\.(css|scss)$": "identity-obj-proxy",
-    "^react-router-dom$": path.join(__dirname, "node_modules/react-router-dom"),
-    "^react-router$": path.join(__dirname, "node_modules/react-router"),
-    "^react-dom/client$": path.join(__dirname, "node_modules/react-dom/client.js"),
-    "^msw/node$": path.join(__dirname, "node_modules/msw/lib/node/index.js"),
+    "^react$": path.join(rootNodeModules, "react/index.js"),
+    "^react/jsx-runtime$": path.join(rootNodeModules, "react/jsx-runtime.js"),
+    "^react/jsx-dev-runtime$": path.join(rootNodeModules, "react/jsx-dev-runtime.js"),
+    "^react-dom$": path.join(rootNodeModules, "react-dom/index.js"),
+    "^react-router-dom$": path.join(rootNodeModules, "react-router-dom"),
+    "^react-router$": path.join(rootNodeModules, "react-router"),
+    "^react-dom/client$": path.join(rootNodeModules, "react-dom/client.js"),
+    "^msw/node$": path.join(rootNodeModules, "msw/lib/node/index.js"),
     "^@mswjs/interceptors/ClientRequest$": path.join(
       interceptorsRoot,
       "ClientRequest/index.cjs",
@@ -29,8 +35,8 @@ module.exports = {
     ),
     "^@mswjs/interceptors/fetch$": path.join(interceptorsRoot, "fetch/index.cjs"),
     "^@mswjs/interceptors/WebSocket$": path.join(
-      __dirname,
-      "node_modules/@mswjs/interceptors/lib/browser/interceptors/WebSocket/index.cjs",
+      rootNodeModules,
+      "@mswjs/interceptors/lib/browser/interceptors/WebSocket/index.cjs",
     ),
   },
   transformIgnorePatterns: [
