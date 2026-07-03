fbb30e5539c0ea0471072625469fa60acd7bfb83
Sam Drumm <sam.drumm@hackney.gov.uk>
2026-07-03 15:56:23 +0100

    fix: change to props as object to meet lint

--- packages/react/src/types/polymorphic.ts
+++ packages/react/src/types/polymorphic.ts
@@ -5,7 +5,10 @@ type IntrinsicProps<C extends React.ElementType> = React.ComponentPropsWithRef<C
 type PropsWithAs<C extends React.ElementType, Props> = IntrinsicProps<C> &
   Props & { as?: C };
 
-export type ForwardRefComponent<DefaultElement extends React.ElementType, Props = {}> = {
+export type ForwardRefComponent<
+  DefaultElement extends React.ElementType,
+  Props extends object = object,
+> = {
   <C extends React.ElementType = DefaultElement>(
     props: PropsWithAs<C, Props & { as?: C }>,
   ): React.ReactElement | null;
